91ed2121f4359545cc3da953e8c005e3eacec2f6
leopeng <peng.liu@dreampix.cn>
2020-04-24 19:26:57 +0800

    1.7

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name='rabbitmq-rpc',
-    version='0.1.6',
+    version='0.1.7',
     packages=find_packages(),
     include_package_data=True,
     description='A modified rabbit-rpc of https://github.com/MidTin/rabbit-rpc',
